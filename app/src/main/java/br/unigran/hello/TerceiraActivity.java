ab58095161080f2d19cc5e364f6eeabb659bd294
Erika <802.282@alunos.unigran.br>
2022-09-28 21:48:57 -0400

    fim da aula

--- app/src/main/java/br/unigran/hello/TerceiraActivity.java
+++ app/src/main/java/br/unigran/hello/TerceiraActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SegundaActivity extends AppCompatActivity {
+public class TerceiraActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_segunda);
-        getIntent().getStringExtra("Nome");
+        setContentView(R.layout.activity_terceira);
     }
 }
