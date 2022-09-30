523836eaecf44a882526a5d1e883dcd3a07a51ed
Erika <802.282@alunos.unigran.br>
2022-09-29 21:31:07 -0400

    2telas

--- app/src/main/java/br/unigran/hello/MainActivity.java
+++ app/src/main/java/br/unigran/hello/MainActivity.java
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView nome;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nome = findViewById(R.id.nome);
+        button = findViewById(R.id.button);
     }
-    public void next (View view){
-        Intent it = new Intent(this, SegundaActivity.class);
-        it.putExtra("Nome", "Erika");
-        startActivity(it);
+    public void confirmEvent (View view){
+        String name = nome.getText().toString();
+        Intent intent = new Intent(this, SegundaActivity.class);
+        intent.putExtra("name", "name");
+        startActivity(intent);
 
     }
 
--- app/src/main/java/br/unigran/hello/SegundaActivity.java
+++ app/src/main/java/br/unigran/hello/SegundaActivity.java
@@ -3,13 +3,20 @@ package br.unigran.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SegundaActivity extends AppCompatActivity {
+    TextView idmsg2;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
-        getIntent().getStringExtra("Nome");
+
+        idmsg2 = findViewById(R.id.nome);
+        name = getIntent().getStringExtra("name");
+
+        idmsg2.setText("Olá," + name);
     }
 }
